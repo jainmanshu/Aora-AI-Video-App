9f7234431d32fe98b883c501b21ac5f826679777
Himanshu Jain <jainmanshu@gmail.com>
2024-09-11 14:56:38 -0500

    adding create page

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,17 +1,56 @@
-import { useAuthActions } from "@convex-dev/auth/react";
-import React from "react";
+import { useAuthActions, useAuthToken } from "@convex-dev/auth/react";
+import { useConvexAuth, useQuery } from "convex/react";
+import { useLocalSearchParams } from "expo-router";
+import { FlatList, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import CustomButton from "../../components/CustomButton";
+import EmptyState from "../../components/EmptyState";
+import SearchInput from "../../components/SearchInput";
+import VideoCard from "../../components/VideoCard";
+import { images } from "../../constants";
+import { api } from "../../convex/_generated/api";
 
 const Profile = () => {
-  const { signOut } = useAuthActions();
+  const auth = useConvexAuth();
+  console.log(auth);
   return (
-    <SafeAreaView className="flex-1 justify-center items-center">
-      <CustomButton
-        title="Sign out"
-        handlePress={() => void signOut()}
-        containerStyles="w-1/2 mt-7"
-      />
+    <SafeAreaView className="bg-primary h-full">
+      {/* <FlatList
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => (
+          <VideoCard
+            title={item.title}
+            creator="Test"
+            thumbnail={item.thumbnail}
+            avatar={images.profile}
+            video={item.video}
+          />
+        )}
+        ListHeaderComponent={() => (
+          <>
+            <View className="flex my-6 px-4">
+              <Text className="font-pmedium text-gray-100 text-sm">
+                Search Results
+              </Text>
+              <Text className="text-2xl font-psemibold text-white mt-1">
+                {query}
+              </Text>
+
+              <View className="mt-6 mb-8">
+                <SearchInput initialQuery={query} />
+              </View>
+            </View>
+          </>
+        )}
+        ListEmptyComponent={() =>
+          !posts.length && (
+            <EmptyState
+              title="No Videos Found"
+              subtitle="No videos found for this search query"
+            />
+          )
+        }
+      /> */}
     </SafeAreaView>
   );
 };
